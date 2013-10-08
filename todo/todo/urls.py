5bcd4dc0d68a67dc8c44b4a8f76643052c37f9ca
Michael Herman <hermanmu@gmail.com>
2013-10-07 22:03:50 -0600

    initial

---
+++ todo/todo/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^$', 'todos.views.home', name='home'),
+    url(r'^delete/$', 'todos.views.delete', name='delete'),
+    url(r'^add/$', 'todos.views.add', name='add'),
+)
